026175c38f8da154318aaa2b131c6a18e335d2f0
Joao Franca <jmcrfp@users.noreply.github.com>
2025-11-26 10:12:29 +0000

    Refactor binary sensor initialization and validation

--- components/trellis_keypad/binary_sensor/__init__.py
+++ components/trellis_keypad/binary_sensor/__init__.py
@@ -1,7 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import binary_sensor
-from esphome.const import CONF_ID, CONF_KEY
+from esphome.const import CONF_KEY
 from .. import TrellisKeypad, trellis_keypad_ns, CONF_KEYPAD_ID
 
 DEPENDENCIES = ["trellis_keypad"]
@@ -10,12 +10,10 @@ TrellisKeypadBinarySensor = trellis_keypad_ns.class_(
     "TrellisKeypadBinarySensor", binary_sensor.BinarySensor
 )
 
-
 def check_button(obj):
     if len(obj[CONF_KEY]) != 1:
         raise cv.Invalid("Key must be one character")
     return obj
-
 
 CONFIG_SCHEMA = cv.All(
     binary_sensor.binary_sensor_schema(TrellisKeypadBinarySensor).extend(
@@ -27,11 +25,12 @@ CONFIG_SCHEMA = cv.All(
     check_button,
 )
 
-
 async def to_code(config):
     var = await binary_sensor.new_binary_sensor(config)
     pad = await cg.get_variable(config[CONF_KEYPAD_ID])
-    # store key char inside the C++ object
+
+    # store key in C++
     cg.add(var.set_key(config[CONF_KEY]))
-    # register button with keypad
+
+    # register the button with the keypad
     cg.add(pad.register_button(var))
